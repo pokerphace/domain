a6f91d52eaa891311640cd01e65922132157a670
Alan Conceição <alan.conceicao@jumia.com>
2020-12-13 03:14:22 +0000

    Adding a getter on test class

--- src/Number.php
+++ src/Number.php
@@ -5,4 +5,9 @@ class Number {
     public function __construct(
         private int|float $number
     ) {}
+
+    public function getNumber(): int|float 
+    {
+        return $this->number;
+    }
 }
